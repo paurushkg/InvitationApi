f218da1c87803798ef5c44bedce7862f1357d2a0
Paurush Kumar Gupta <gpaurush39@gmail.com>
2021-05-19 17:32:25 +0530

    Created some models

---
+++ api/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import *
+
+admin.site.register(Groom)
+admin.site.register(Bride)
+admin.site.register(Event)
+admin.site.register(Relative)
+
---
+++ api/core/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.conf import settings
+
+
+class Relative(models.Model):
+    name = models.CharField(max_length=127)
+    message = models.CharField(max_length=255)
+    address = models.CharField(max_length=127)
+    host = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Bride(models.Model):
+    name = models.CharField(max_length=255)
+    father = models.CharField(max_length=255)
+    mother = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        self.name
+
+
+class Groom(models.Model):
+    name = models.CharField(max_length=255)
+    father = models.CharField(max_length=255)
+    mother = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        self.name
+
+
+class Event(models.Model):
+    host = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    date_and_time = models.DateTimeField()
+    bride = models.ForeignKey(to=Bride,
+                              on_delete=models.CASCADE,
+                              null=False,
+                              unique=True,
+                            )
+    groom = models.ForeignKey(to=Groom,
+                              on_delete=models.CASCADE,
+                              null=False,
+                              unique=True,
+                            )
+
+    def __str__(self):
+        return self.host.name
+
